cff5b77bae8a4e05e9b5c20360f2190ff1f90acb
ndungutse-maker <dav.ndungutse@gmail.com>
2021-12-12 15:00:20 +0200

    Setting up Fontawsome and PdfMake

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,9 @@ import './components/modals';
 import './components/accountPage';
 import './controller/authentication';
 import './controller/departure';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 import { capitalizeStr } from './utils/str';
 
 export const state = {};
